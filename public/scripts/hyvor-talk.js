e0ca167013b0ae6879ebe54d4bb25b3c2444bf66
K. M. Jones-Eastland <kmje405@gmail.com>
2026-01-27 16:48:37 -0600

    Update Hyvor Talk script source and attribute for improved integration

--- public/scripts/hyvor-talk.js
+++ public/scripts/hyvor-talk.js
@@ -1,8 +1,8 @@
 // Hyvor Talk initialization script
 window.addEventListener("load", () => {
   const s = document.createElement("script");
-  s.src = "https://talk.hyvor.com/embed.js";
+  s.src = "https://talk.hyvor.com/embed/embed.js";
   s.async = true;
-  s.setAttribute("data-hyvor-talk-id", "14933");
+  s.setAttribute("data-website-id", "14933");
   document.body.appendChild(s);
 });
