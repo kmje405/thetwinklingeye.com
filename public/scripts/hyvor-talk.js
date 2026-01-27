f75f4b10c624693189e5d0036c1b28b9dd1f8901
K. M. Jones-Eastland <kmje405@gmail.com>
2026-01-27 16:46:23 -0600

    Move Hyvor Talk script initialization to a separate file for better organization and maintainability

---
+++ public/scripts/hyvor-talk.js
@@ -0,0 +1,8 @@
+// Hyvor Talk initialization script
+window.addEventListener("load", () => {
+  const s = document.createElement("script");
+  s.src = "https://talk.hyvor.com/embed.js";
+  s.async = true;
+  s.setAttribute("data-hyvor-talk-id", "14933");
+  document.body.appendChild(s);
+});
